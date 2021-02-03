950b7c31287dc1b9d36653bfbb18f735931e0a75
Riwal Le Faou <riwal.lefaou7@gmail.com>
2021-02-03 16:25:11 +0100

    Add cors policy and last arrosage date in mock

--- back/index.js
+++ back/index.js
@@ -1,32 +1,37 @@
 var express = require('express');
 var app = express();
 var startCron = require("./cron.js")
-
+var cors = require('cors')
+app.use(cors())
 app.get('/stats', function(req, res) {
     res.send(JSON.stringify([
         {
             time: "2021-01-26T13:00:50.081Z",
             photosensor: 834,
             humidity: 430,
-            watering: false
+            watering: false,
+            lastArrosageDate: "2021-01-15T13:00:50.081Z"
         },
         {
             time: "2021-01-26T13:15:50.081Z",
             photosensor: 834,
             humidity: 430,
-            watering: false
+            watering: false,
+            lastArrosageDate: "2021-01-15T13:00:50.081Z"
         },
         {
             time: "2021-01-26T13:30:50.081Z",
             photosensor: 834,
             humidity: 430,
-            watering: false
+            watering: false,
+            lastArrosageDate: "2021-01-15T13:00:50.081Z"
         },
         {
             time: "2021-01-26T13:45:50.081Z",
             photosensor: 834,
             humidity: 430,
-            watering: false
+            watering: false,
+            lastArrosageDate: "2021-01-15T13:00:50.081Z"
         }
     ]));
 });
@@ -36,4 +41,4 @@ app.get('/actions', function(req, res) {
 });
 
 startCron()
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
